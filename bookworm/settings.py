bbaad39512a36dbd36315ad5c4dd9d92d93d126c
unknown <mukeshsharma31994@gmail.com>
2022-03-08 23:27:37 +0000

    setting and url file changed

--- bookworm/settings.py
+++ bookworm/settings.py
@@ -136,6 +136,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+# MEDIA_ROOT = [os.path.join(BASE_DIR ,'media')]
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
